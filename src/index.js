435190b150b15ce901762a5cf498ccf8f388161c
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-01-14 08:28:15 -0800

    split icosahedron

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import * as THREE from 'three';
+const THREE = require('three');
+
+window.THREE = THREE;
+require('three/examples/js/controls/OrbitControls');
 
 const canvas = document.querySelector('#three');
 
@@ -6,20 +9,42 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.setZ(5);
+camera.position.setZ(2);
 
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enablePan = false;
+controls.enableDamping = true;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  polygonOffset: true,
+  polygonOffsetFactor: 1,
+  polygonOffsetUnits: 1,
+});
+const geometryBeforeSplitting = new THREE.IcosahedronGeometry(1, 1);
+const geoBSVerts = geometryBeforeSplitting.vertices;
+const geoBSFaces = geometryBeforeSplitting.faces;
+geoBSFaces.forEach((face) => {
+  const splitGeo = new THREE.Geometry();
+  splitGeo.vertices.push(geoBSVerts[face.a], geoBSVerts[face.b], geoBSVerts[face.c]);
+  const newFace = face;
+  newFace.a = 0;
+  newFace.b = 1;
+  newFace.c = 2;
+  splitGeo.faces.push(newFace);
+  const mesh = new THREE.Mesh(splitGeo, material);
+  const wireGeometry = new THREE.EdgesGeometry(geometryBeforeSplitting);
+  const wireMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+  const wireframe = new THREE.LineSegments(wireGeometry, wireMaterial);
+  mesh.add(wireframe);
+  scene.add(mesh);
+});
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  controls.update();
   renderer.render(scene, camera);
 }
 
